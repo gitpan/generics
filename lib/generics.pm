f7780dcf71de62f9ca06f71d68da1fe75dd693bc
Stevan Little <stevan@cpan.org>
2004-04-19 05:03:06 +0000

    Import of STEVAN/generics-0.03 from CPAN.
    gitpan-cpan-distribution: generics
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         STEVAN/generics-0.03.tar.gz
    gitpan-cpan-author:       STEVAN
    gitpan-cpan-maturity:     released

--- lib/generics.pm
+++ lib/generics.pm
@@ -3,7 +3,7 @@ package generics;
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 sub import {
 	my ($self) = shift;
@@ -161,32 +161,96 @@ __END__
 
 =head1 NAME
 
-generics - pragma for adding generic parameters to modules
+generics - pragmatic module for perl-ish generics
 
 =head1 SYNOPSIS
 
-    use generics;
+    package MyModule;
     
-    # for use from within a class
-    use generics params => qw(PARAMETER);
+    # for use from within a package
+    use generics params => qw(
+                PARAMETER
+                );
     
-    # another use from within a class
-    use generics default_params => (PARAMETER => "A value");
+    # another use from within a package
+    # and setting a default value for a
+    # parameter
+    use generics default_params => (
+                PARAMETER => "A value"
+                );
+                
+    # ... outside of the MyModule package 
     
-    # using it from outside a class 
-    # to change the classes parameters
-    use generics MyModule => (PARAMETER => "A new value");
+    # using it from outside a package
+    # to change the packages parameters
+    use generics MyModule => (
+                PARAMETER => "A new value"
+                );
+                
+    # ... elsewhere in the source            
     
-    # see DESCRIPTION below for a better understanding of this module
+    package MyDerivedModule;
+    
+    use base qw(MyModule);
+    
+    # generic parameters can be inherited too
+    use generics inherit => "MyModule";
+    
+    # see DESCRIPTION below for a better understanding 
+    # of this module and how it can be used
 
 =head1 DESCRIPTION
 
-Many languages incorporate the concept of generic programming, specifically C++ and Ada. The generics pragma was inspired by these languages, but because of Perl's existing type flexibility it does not need generic programming per say. Instead I took the idea of passing generic parameters into a pre-existing class as a form of class configuration. The same generic programming can be accomplished to a certain degree, although because generics parameters are assigned at compile time they become part of the class rather than the instance (as in most generics situations). 
+=head2 What are generics?    
 
-So all this said, at their heart, generics are class configuration parameters. They can get trickier depending on how you choose to use them. The easiest example is that of a "Session" object. Here is some sample code:
+Generics and generic programming is style of programming which aims to make more generalized software compontents. It is more prevelant in strongly typed langauges like Ada95, C++ (through the STL) and more recently in C# 2.0 and Java 1.5. On a very simple level, generics allow these langauges to achieve a level of type polymorphism where they didn't have that before. Here is a simplistic pseudo-C++/C#/Java-style example of generics:
 
-    # be sure to load the 
-    # object's module
+  class Stack : <T> {
+    _stack = array of T;
+  
+    T pop { ... }
+    void push (T item) { ... }
+  }
+  
+  // create the Stack instance with 
+  // a type parameter here
+  Stack<int> my_stack = new Stack<int> 
+
+The C<T> is the generic parameter, which represents a generic type that the stack holds. When we create an instance of C<Stack>, the generic type parameter is assigned and the object is actually then of type C<Stack<int>>, which is different than say, C<Stack<float>>. 
+
+This is a very limited and simple type of generics, languages like Ada 95 support a more extensive version of generics which allow not only for the defferment of types, but also of values, functions and procedures within the Ada package. Here is an example of a generic Stack package in Ada 95:
+
+  generic
+    -- "private" means essentially "any type at all"
+    type Item_Type is private;
+    -- our Stack need a size, which is an integer 
+    -- and here we default it to 100
+    Size : Integer := 100;
+  package Stack is
+    -- ... implementation here
+  end Stack;
+
+  -- create a new "instance" of the Stack package
+  -- that uses integers as its type, and whose size
+  -- is defaulted to 100
+  package Int_Stack is new Stack (Item_Type => Integer);
+
+Here we created not only a generic type for the Stack to hold, but also allowed the size of the stack to be determined, but also have a default. You can also make a function or procedure a generic parameter in Ada. 
+
+Much of what is going on behind the scenes with these generics is that the compiler is able to retain the langauges type-safety while still providing a facility for type-polymorphism. Like I said, this is usually something needed by strongly-typed languages, and more specifically, those of the "type-checking" variety.  By now you are wondering what does all this have to do with perl? 
+
+=head2 Generics in perl
+
+Perl is not strongly typed, and it doesn't really do any type-checking, and will do type-casting when neseccary. It's (sort of) type-polymorphic as well, meaning you can treat most all of your types reasonably the same.  Sure things like C<==> and C<!=> vs. C<eq> and C<ne> can be annoying, but they are surrmountable problems. So why would I need generics in perl you say?
+
+This module aims to provide a perl-ish form of generics, which is similar too, but not exaclty like what I desribe above. Perl doesn't really need the type flexibility of generics, but it can use the more advanced features of Ada 95 style generics, such as defining values and functions as generic parameters. Since we do not need the compiler optimizations gotten by declaring the generic parameters on of each of our instances, we can define them for the specific package itself rather than just an instance of it.
+
+It is possible to view this pragma as providing the facility to manage package configuration parameters. It was actually inspired by the C<constant> pragma, and the desire to assign those constants across package boundries.
+
+=head2 How do I use generics?
+
+The easiest way to explain the details behind how this module works, and how one would work with it, through examples. This example is that of a "Session" object. Here is some sample code:
+
     use Session;
     
     # set the generic params
@@ -198,37 +262,26 @@ So all this said, at their heart, generics are class configuration parameters. T
     # create a Session object instance
     my $s = Session->new();
 
-Generics are used here as a way of configuring the Session object to have a 30 minute timeout period and generate a session id that is 20 characters long. Any Session object you create after the use generics  declaration will have those configuration parameters available to them. 
+Generics are used here as a way of configuring the Session object to have a 30 minute timeout period and generate a session id that is 20 characters long. Any Session object you create after the C<use generics> declaration will utilize those configuration parameters. 
 
-The generics module is actually what is called a compiler pragma. Traditionally a pragma is kind of a suggestion made to the compiler so that it might perform some certain kind of optimizations on the code it is compiling. In Perl, pragmas are usually specialized modules that the compiler executes during the compilation process, and are more akin to macros.
+While it would be just as simple to just add the parameters to the Session object constructor and configure it each time, this way is actually cleaner and faster. It is cleaner, because you need not have to remember the parameters each time you create a new Session object (especially since they are unlikely to change throughout the life of your application). And it is faster because the use generics declaration will actually set the parameters during the compilation of the Session object, and not at run-time when you create the object.
 
-While it would be just as simple to just add the parameters to the Session object constructor (new) and configure it each time, this way is cleaner and faster. It is cleaner, because you need not have to remember the parameters each time you create a new Session object (especially since they are unlikely to change throughout the life of your application). And it is faster because the use generics declaration will actually set the parameters during the compilation of the Session object, and not at run-time when you create the object.
+The only drawback to the compilation time configuration is that once the module is compiled, those values are set. Of course this is not a drawback if you do not plan on changing the parameters, and want them to stay as they are through the life of your application. If your generics are designed well you will never have a need to change the parameters during runtime. If however you do need to change things are runtime, there is a way with the C<change_params> method (see below). 
 
-The only drawback to the compilation time configuration is that once the module is compiled, those values are set. Of course this is not a drawback if you do not plan on changing the parameters, and want them to stay as they are through the life of your application. If your classes are designed well you will never have a need to change the parameters during runtime. 
+=head3 Configuring generics for a package
 
-If however you do need to change things are runtime, there is a way. This method should only be used as a last resort however. Here is an example:
-
-    generics->change_params(Session => (
-                SESSION_TIMEOUT => 10, 
-                SESSION_ID_LENGTH => 20
-                ));
-
-You do not have to re-use the generics module, as a matter of fact, if you do, you will get some weird results. This should only be used in extreme circumstances, when you have determined there is no other way that will work. It does not restore the default params either, it just changes the already existing ones.
-
-There is also another side to generics. The side that lives within the actual class you are attempting to configure.
-
-Here are 2 examples:
+There is also another side to generics. The side that lives within the actual package you are attempting to configure. There a few options available to you here. First is to set paramters without defaults. Here is an example:
 
     package Session;
     
     use generics params  => qw(
-		SESSION_TIMEOUT
+        SESSION_TIMEOUT
         SESSION_ID_LENGTH
         );
 
-In order for a class to be configured with generics, you must first specify what those parameters are in the class itself. The above statement does just that. The Session object will now only accept those two parameters, and throw an exception otherwise.
+This will tell generics the parameters available within the package, and create stub subroutines for them. These stub routines will throw an exception if they are called before they are configured. However, these stub routines make it possible to use the parameters in your code and allow the module to still compile and run with both strict and warnings on.
 
-Here is the other example:
+The other option, is to assign defaults to the parameters in the package itself. Here is an example:
 
     package Session;
     
@@ -237,36 +290,67 @@ Here is the other example:
         SESSION_ID_LENGTH => 20
         );
 
-This example does just what the previous one does in terms of setting up the valid parameters for the Session object, with one difference. It assigns default parameters. Without the default parameters, the Session object would not work, and throw an exception at runtime. With the defaults, you can skip the use generics Session part and the class would just use the installed default values. Also, with defaults, you can choose to only set as many params as you need. Here is an example:
+This example does just what the previous one does in terms of setting up the valid parameters for the Session package, but it also assigns default parameters. With the defaults, you don't have to do anything and the package would just use the installed default values. But using defaults, you have another option, which is to set as many params as you want/need. For instance:
 
     use generics Session => (SESSION_TIMEOUT => 120);
 
-This code will utilize the default setting for the SESSION_ID_LENGTH param, but change the SESSION_TIMEOUT param to be 120 minutes. 
+This code will utilize the default setting for the C<SESSION_ID_LENGTH> param, but change the C<SESSION_TIMEOUT> param to be 120 minutes. 
 
-Another important note is that you can make anything a generic parameter. The following bit of code is a  valid use of generics:
+=head3 Types of generic parameters
+
+Up until now, our generic parameters have been plain scalar values, but they can also be scalar reference variables as well. This means hash references, array references and also subroutine references. The following bit of code is a  valid use of generics:
 
     use generics Session => (
-		SESSION_TIMEOUT => sub {
-                if (Date->now()->getDayOfWeek() eq "Wednesday") {
+        SESSION_TIMEOUT => sub {
+                if (((rand() * 100) % 2) == 0) {
                     return 30;
                 }
                 else {
                     return 120;
                 }
-        },
+            },
         SESSION_ID_LENGTH => 20
         );
 
-The above code uses an anonymous subroutine and the Date object (which can be found in the Utilities category in the iI::Framework) to get the day of the week and if it is Wednesday, it sets the session timeout to 30 minutes, otherwise it sets it to 120. This of course is kind of silly, but it just illustrates that you have alot of flexibility with generics. It is even possible to have an entire method of your class defined with generics if you are so inclined.
+The above code uses an anonymous subroutine, which will set the session timeout to 30 minutes if a random number is even, otherwise it sets it to 120. This of course is kind of silly, but it just illustrates that you have alot of flexibility with generics.
 
-The parameters of the Session object can be used much like constants within the class code itself. So for example you might do something like this:
+It is worth noting that there is no type-checking done by generics, so it is possible to set a default to one type, and then assign it to a different type. More than likely this will change in subsequent versions of this module, so exploiting this is not recommended. 
 
-    sub createSessionId {
-        my @chars = (a .. z, A .. Z, 0 .. 9);
-        return map { $chars[rand()] } (0 .. SESSION_ID_LENGTH);
-    }
+When using the generic parameters in the package, they should be used just like constants that are created with the C<constant> pragma. They are actually just subroutines which the perl compiler will inline. It is important to keep this in mind when using the parameters inside your package code, in particular in situations where your parameter would be interpreted as a string (ex: a hash key, interpolated into a string). Here is an example of how it might be used in the Session package:
 
-Now whenever you call createSessionId you can be confident that it will return a string exactly as long as you specified (or not specified if the default_params was used).
+    sub getSessionId {
+        my @chars = ('a' .. 'z', 'A' .. 'Z', 0 .. 9);
+        return join "" => map { 
+                    $chars[((rand() * 100) % scalar @chars)] 
+                    } (1 .. SESSION_ID_LENGTH);
+    } 
+
+=head3 Generics and OOP
+
+The generics pragma also is built with Object Oriented programming in mind. Since the parameters created are actually subroutines, they are easily inherited with only a few changes.
+
+Inheritance of generic parameters is not automatic. In order to inherit the same generic parameters as your base class, you must tell generics to do so, like this:
+
+    package MySession;
+    
+    use base qw(Session);
+    
+    use generics inherit => "Session";
+
+What this does is to actually duplicate the available generic parameters in the derived class, allowing them to be assigned to seperately from the base class. But up until those parameters are assigned, they will be aliases to those in the base class. Meaning that your derived class B<will> inherit any and all changes in the base classes generic parameters. Once you assign them for your derived class though, you have disolved that relationship. This allows the default parameters to be carried over from base class to the derived class. 
+
+It is worth nothing that generics will not actually test to see if your package actually inherits from the class you say it does. This is because all our generics happens at compile time, and we can not know for sure at that time what your base class might be. We go on faith that you will do only what makes sense. 
+
+It is also important to note that your generic parameters will usually not be virtual. Meaning that in the example above of the C<getSessionId> method, the C<SESSION_ID_LENGTH> parameter will be that of the base class if the C<getSessionId> method itself is not overridden. If this is not your desired behavior, then it is easily changed with one small addition to the code to make the parameter work properly as a virtual method.
+
+    sub getSessionId {
+        my @chars = ('a' .. 'z', 'A' .. 'Z', 0 .. 9);
+        return join "" => map { 
+                    $chars[((rand() * 100) % scalar @chars)] 
+                    } (1 .. $self->SESSION_ID_LENGTH);
+    } 
+
+This is not considered a feature, although it can be desireable behavior at times. However it may be fixed in subsequent versions of this module, so reliance upon it is not suggested. 
 
 =head1 METHODS
 
@@ -285,6 +369,18 @@ This method is a predicate, returning true (1) if the C<$package> has generic pa
 =item B<dump_params ($package)>
 
 This will dump a hash of the generic parameters. One important thing to note is that it will execute the parameters, so this may not be very useful for subtroutine ref parameters.
+
+=back
+
+=head1 TO DO
+
+=over 5
+
+=item * Make inherited generics proper virtual method citizens. 
+
+=item * Possibly add type-checking to generic parameters.
+
+=item * Possibly for mixed params and default params in the same package.
 
 =back
 
@@ -316,7 +412,9 @@ I use B<Devel::Cover> to test the code coverage of my tests, below is the B<Deve
 
 =head1 SEE ALSO
 
-Nothing I can think of yet. But this module was inspired by the 'constant' pragma, and the desire to assign those constants across module lines. It borrows some of its ideas from other languages, in particular Ada and C++/STL, although our generics are not instance oriented as theirs are. 
+Nothing I can think of yet. But this module was inspired by the C<constant> pragma, and the desire to assign those constants across module lines. It borrows some of its ideas from other languages, in particular Ada and C++/STL, although our generics are not instance oriented as theirs are. 
+
+If you want to learn more about generics and generic programming, just Googling the phrase "generic programming" produces many useful links.
 
 =head1 AUTHOR
 
